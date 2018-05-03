1a9c20bd4405cc9c483a12e690d9a94354e8ca56
yuubd <gc.kim93@hotmail.com>
2018-05-03 16:14:54 -0700

    reactions folder added

---
+++ src/fb_eactions/Reactions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Reactions extends Component {
+  render() {
+    return (
+      <div>this is reaction components</div>
+    );
+  }
+}
+
+export default Reactions;
