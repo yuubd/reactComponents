1f08998171dec4b040500abe8ea571439dc68a9f
yuubd <gc.kim93@hotmail.com>
2018-05-03 17:29:18 -0700

    save point1 before ejecting

---
+++ src/fb_reactions/Reactions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Reactions.scss'
+
+class Reactions extends Component {
+  render() {
+    return (
+      <body>
+        <div className="wrap">
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+        </div>
+      </body>
+    );
+  }
+}
+
+export default Reactions;
