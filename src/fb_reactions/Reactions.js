9be8065580d5113dd64db3c87b96e5b35608218a
yuubd <gc.kim93@hotmail.com>
2018-05-03 19:16:07 -0700

    sass configuration added and UI and hovering effects of reactions finished

--- src/fb_reactions/Reactions.js
+++ src/fb_reactions/Reactions.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import './Reactions.scss'
+import './Reactions.css'
+import angry from './public/facebook-angry.svg'
+import haha from './public/facebook-haha.svg'
+import like from './public/facebook-like.svg'
+import love from './public/facebook-love.svg'
+import sad from './public/facebook-sad.svg'
+import wow from './public/facebook-wow.svg'
+
 
 class Reactions extends Component {
   render() {
     return (
       <body>
         <div className="wrap">
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
+          <div className="circle"><img src={like} alt="logo"/>
+            <p>like</p>
+          </div>
+          <div className="circle"><img src={love} alt="logo"/>
+            <p>love</p>
+          </div>
+          <div className="circle"><img src={haha} alt="logo"/>
+            <p>haha</p>
+          </div>
+          <div className="circle"><img src={wow} alt="logo"/>
+            <p>wow</p>
+          </div>
+          <div className="circle"><img src={sad} alt="logo"/>
+            <p>sad</p>
+          </div>
+          <div className="circle"><img src={angry} alt="logo"/>
+            <p>angry</p>
+          </div>
         </div>
       </body>
     );
