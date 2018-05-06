1144615b50cf6a58a971c89755998e0547168693
yuubd <gc.kim93@hotmail.com>
2018-05-06 12:36:21 -0700

    calculator skeleton files

---
+++ src/calculator/Calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div>
+        <p>Calculator</p>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
